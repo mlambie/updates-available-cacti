402fe7061311eb6f335a0fd8971150f33fb35848
Matthew Lambie <mlambie@thefrontiergroup.com.au>
2010-10-27 09:14:13 +0800

    Added UA_ variable prefix and cating file instead of exec

--- updates.sh
+++ updates.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-UPDATE="/usr/lib/update-notifier/update-motd-updates-available"
-TOTAL=0
-SEC=0
-NONSEC=0
+UPDATE_AVAIL="/var/lib/update-notifier/updates-available"
 
-if [ -x $UPDATE ]; then
-  TOTAL="$(${UPDATE} | grep "packages can be updated." | cut -f1 -d" ")"
-  [ -z $TOTAL ] && TOTAL=0
-  SEC="$(${UPDATE} | grep "updates are security updates." | cut -f1 -d" ")"  
-  [ -z $SEC ] && SEC=0
-  NONSEC="$(($TOTAL - $SEC))"
+if [ -e "$UPDATE_AVAIL" ]; then
+  UA_TOTAL="$(cat ${UPDATE_AVAIL} | grep "packages can be updated." | cut -f1 -d" ")"
+  UA_SEC="$(cat ${UPDATE_AVAIL} | grep "updates are security updates." | cut -f1 -d" ")"  
 fi
-echo "total:${TOTAL} sec:${SEC} nonsec:${NONSEC}"
+
+[ -z $UA_TOTAL ] && UA_TOTAL=0
+[ -z $UA_SEC ] && UA_SEC=0
+UA_NONSEC="$(($UA_TOTAL - $UA_SEC))"
+printf "${UA_TOTAL}\n${UA_SEC}\n${UA_NONSEC}"
